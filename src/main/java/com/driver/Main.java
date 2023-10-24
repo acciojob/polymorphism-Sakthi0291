4b38cbaa1474d01e421838d7ebf108df31958938
Sakthi0291 <sbb357753@gmail.com>
2023-10-24 14:45:07 +0530

    new changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,35 @@
 package com.driver;
 
 public class Main {
+    public void main(String args[])
+    {
+        Product p = new Product();
+
+        int ans1 = p.product(5,2);
+        int ans2 = p.product(5,2,3);
+        double ans3 = p.product(1.0,2.0);
+
+        System.out.println(ans1+" "+ans2+" "+ans3+" ");
+
+    }
+
+    class Product
+    {
+        public int product(int x, int y)
+        {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z)
+        {
+            return x*y*z;
+        }
+
+        public double product(double x, double y)
+        {
+            return x*y;
+        }
+    }
+
 
 }
